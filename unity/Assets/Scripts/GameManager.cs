3c0f4d35ea9ffa8fc2ff4bf9b841c6765b48f75f
Jeppe Lindskov <jeppelind@hotmail.com>
2019-04-27 20:05:57 +0200

    GameManger Updates and a soundManager Added
    title
    .

--- unity/Assets/Scripts/GameManager.cs
+++ unity/Assets/Scripts/GameManager.cs
@@ -4,6 +4,56 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance = null;
+
+
+    public GameObject playerOne;
+    public GameObject playerTwo; 
+
+    public int playerOneDeaths;
+    public int playerTwoDeath; 
+    
+    public enum GameState
+    {
+        MAINMENU, 
+        PLAYING,
+        WINSCREEN,
+    }
+    public  GameState currentGamestate;
+
+    public bool gameIsStarted = false; 
+    
+    
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+        
+        playerOneDeaths = 0;
+        playerTwoDeath = 0;
+
+        currentGamestate = GameState.MAINMENU;
+
+    }
+
+    public void StartGame()
+    {
+        currentGamestate = GameState.PLAYING; 
+    }
+
+
+
+    public void GameOver()
+    {
+        currentGamestate = GameState.WINSCREEN; 
+    }
+
     public void SteppedOnWalkableTile(Collider tile, PlayerMovement actor)
     {
         ;
@@ -12,5 +62,21 @@ public class GameManager : MonoBehaviour
     public void SteppedOnNonWalkableTile(Collider tile, PlayerMovement actor)
     {
         actor.Die();
+       
     }
+
+
+    public void RegisterPlayer(GameObject playerOne, GameObject playerTwo)
+    {
+        playerOne = this.playerOne;
+        playerTwo = this.playerTwo; 
+
+    }
+    
+    
+
+    
+    
+    
+    
 }
---
+++ unity/Assets/Scripts/Sound/SoundManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using FMOD;
+using UnityEngine;
+using FMOD.Studio;
+using FMODUnity;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager instance = null;
+
+    [EventRef]
+    public string buttonHover;
+    
+    [EventRef]
+    public string buttonClick;
+
+    [EventRef]
+    public string musicEvent;
+
+    private PARAMETER_ID m_gameStateParameter;
+    private EventInstance m_musicInstance; 
+
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        
+        m_musicInstance = FmodEvent.Play(musicEvent, transform, null);
+        m_gameStateParameter = FmodEvent.GetParameterId(musicEvent, "GameState");
+    }
+
+
+    private void Update()
+    {
+
+        switch (GameManager.Instance.currentGamestate)
+        {
+            case GameManager.GameState.MAINMENU:
+                m_musicInstance.setParameterByID(m_gameStateParameter,0);
+                break;
+            case GameManager.GameState.PLAYING:
+                m_musicInstance.setParameterByID(m_gameStateParameter, 1);
+                break;
+            case GameManager.GameState.WINSCREEN:
+                m_musicInstance.setParameterByID(m_gameStateParameter, 5);
+                break;
+            default:
+                m_musicInstance.setParameterByID(m_gameStateParameter,0);
+                break; 
+        }
+    }
+}
