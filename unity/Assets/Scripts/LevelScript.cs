2850f728c4f5e5d5c476907d24ecef107a83916f
Lasse Hansen <lasse.j.hansen@gmail.com>
2019-04-27 10:27:50 +0200

    level generator

---
+++ unity/Assets/Scripts/LevelScript.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelScript : MonoBehaviour
+{
+
+	public int levelWidth;
+	public int levelHeight;
+
+	public GameObject walkableTile;
+	public GameObject notWalkableTile;
+
+	private Color[] tileColors;
+
+	public Color walkableColor;
+	public Color notWalkableColor;
+
+	public Texture2D levelTexture;
+
+
+	void Start ()
+	{
+		levelWidth = levelTexture.width;
+		levelHeight = levelTexture.height;
+		LoadLevel ();	
+	}
+	
+
+	void LoadLevel ()
+	{
+		tileColors = new Color[levelWidth * levelHeight];
+		tileColors = levelTexture.GetPixels ();
+
+		for (int y = 0; y < levelHeight; y++) 
+		{
+			for (int x = 0; x < levelWidth; x++) 
+			{
+				if (tileColors [x + y * levelWidth] == walkableColor) 
+				{	
+					Instantiate (walkableTile, new Vector3 (x, y), Quaternion.identity);	
+				}
+
+				if (tileColors [x + y * levelWidth] == notWalkableColor) 
+				{	
+					Instantiate (notWalkableTile, new Vector3 (x, y), Quaternion.identity);
+				}			
+			}
+		}
+	}
+}
