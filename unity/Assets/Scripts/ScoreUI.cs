54220f45d443406a25166c652c8e5affda021e7f
Jeppe Lindskov <jeppelind@hotmail.com>
2019-04-27 23:11:44 +0200

    UI DeathCounter Added

---
+++ unity/Assets/Scripts/ScoreUI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreUI : MonoBehaviour
+{
+
+    public TextMeshProUGUI playerOneDeaths;
+    public TextMeshProUGUI playerTwoDeaths;
+
+    private PlayerMovement playerOne;
+    private PlayerMovement playerTwo;
+
+
+    private void Start()
+    {
+        playerOne = GameManager.Instance.playerOne.GetComponent<PlayerMovement>();
+        playerTwo = GameManager.Instance.playerTwo.GetComponent<PlayerMovement>();
+    }
+
+    void Update()
+    {
+        playerOneDeaths.text = playerOne.deathCount.ToString();
+        playerTwoDeaths.text = playerTwo.deathCount.ToString(); 
+
+    }
+}
