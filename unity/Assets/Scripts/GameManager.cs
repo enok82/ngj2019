3b6b927d95de83e014617ace1fa958aaffa93496
Stefan <stefan.enoksson@gmail.com>
2019-04-27 17:09:58 +0200

    Moved to RigidBodies

---
+++ unity/Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public void SteppedOnWalkableTile(Collider tile, PlayerMovement actor)
+    {
+        ;
+    }
+
+    public void SteppedOnNonWalkableTile(Collider tile, PlayerMovement actor)
+    {
+        actor.Die();
+    }
+}
