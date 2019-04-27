c1177a0ece66ff2f8b1fa103916237b7067a3480
Lasse Hansen <lasse.j.hansen@gmail.com>
2019-04-27 20:25:25 +0200

    BEUTIFU LBABY BOY MANIN MENYYNUN

---
+++ unity/Assets/DanceScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DanceScript : MonoBehaviour
+{
+    private int danceNo;
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        danceNo = Random.Range(0, 6);
+        anim = GetComponent<Animator>();
+        anim.SetInteger("Dance", danceNo);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
