bce9bad326ff2991bcfb3e2b10346f85c6bce732
Stefan <stefan.enoksson@gmail.com>
2019-04-27 13:44:41 +0200

    wip

---
+++ unity/Assets/CollisionScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionScript : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Boom" + other.tag + tag);
+    }
+}
