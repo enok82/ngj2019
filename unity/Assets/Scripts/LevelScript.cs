55ffa0ed6ffb3f214edb781a928e8179d8058b46
Lasse Hansen <lasse.j.hansen@gmail.com>
2019-04-28 10:13:23 +0200

    levelscript fix

--- unity/Assets/Scripts/LevelScript.cs
+++ unity/Assets/Scripts/LevelScript.cs
@@ -52,9 +52,9 @@ public class LevelScript : MonoBehaviour
 
 	void LoadLevel ()
 	{
-        levelTexture = levelTextures[Random.Range(0,levelTextures.Length)];
+        /*levelTexture = levelTextures[Random.Range(0,levelTextures.Length)];
         levelWidth = levelTexture.width;
-        levelHeight = levelTexture.height;
+        levelHeight = levelTexture.height;*/
 
         tileColors = new Color[levelWidth * levelHeight];
 		tileColors = levelTexture.GetPixels ();
@@ -91,6 +91,11 @@ public class LevelScript : MonoBehaviour
                 }
             }
 		}
+        /*foreach (GameObject tile in walkableTiles)
+        {
+            Renderer renderer = tile.GetComponent<Renderer>();
+            renderer.material.EnableKeyword("_EmissionMap");
+        }*/
 	}
 
 
